8aa44bf18799d8f7da2f682e6190766b4bf922d2
IGYEONGMUN <doehdotn2@naver.com>
2025-01-02 17:10:25 +0900

    250102 bulletupdate

--- main.js
+++ main.js
@@ -10,6 +10,20 @@ let backgoundImage, spaceshipImage, bulletImage, enemyImage, gameOverImage;
 let spaceshipX = canvas.width / 2 - 32;
 let spaceshipY = canvas.height - 64;
 
+let bulletList=[]
+function Bullet(){
+  this.x=0
+  this.y=0
+  this.init=function(){
+    this.x=spaceshipX+18
+    this.y=spaceshipY-10
+
+    bulletList.push(this)
+  }
+  this.update=function(){
+    this.y-=7
+  }
+}
 function loadImage() {
   backgoundImage = new Image();
   backgoundImage.src = "imgs/spaceback.avif";
@@ -32,10 +46,20 @@ function setupKeyboardListener() {
   document.addEventListener("keydown", function (event) {
     keysDown[event.keyCode] = true;
   });
-  document.addEventListener("keyup", function () {
+  document.addEventListener("keyup", function (event) {
     delete keysDown[event.keyCode];
+
+    if(event.keyCode==32){
+      createBullet()
+    }
   });
 }
+
+function createBullet(){
+  let b=new Bullet()
+  b.init()
+}
+
 function update() {
   if (39 in keysDown) {
     spaceshipX += 5;
@@ -49,11 +73,18 @@ function update() {
   if (spaceshipX >= canvas.width - 64) {
     spaceshipX = canvas.width - 64;
   }
+  for(let i=0;i<bulletList.length;i++){
+    bulletList[i].update()
+  }
 }
 
 function render() {
   ctx.drawImage(backgoundImage, 0, 0, canvas.width, canvas.height);
   ctx.drawImage(spaceshipImage, spaceshipX, spaceshipY);
+
+  for(let i=0;i<bulletList.length;i++){
+    ctx.drawImage(bulletImage,bulletList[i].x,bulletList[i].y)
+  }
 }
 
 function main() {
