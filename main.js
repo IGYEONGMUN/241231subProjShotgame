56598635f4bc5cb10e7b8066a367562e7a594b09
IGYEONGMUN <doehdotn2@naver.com>
2025-01-02 21:21:37 +0900

    full setting

--- main.js
+++ main.js
@@ -76,6 +76,10 @@ function update() {
   for(let i=0;i<bulletList.length;i++){
     bulletList[i].update()
   }
+  for(let i=0;i<enemyList.length;i++){
+    enemyList[i].update()
+  }
+ 
 }
 
 function render() {
